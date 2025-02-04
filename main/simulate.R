101f6b563a30f64b1e9d15695592af8a93fe99fa
Kohei Kawaguchi <kohei.kawagucci@gmail.com>
2025-02-04 12:58:58 +0800

    added report

--- main/simulate.R
+++ main/simulate.R
@@ -8,6 +8,13 @@ set.seed(1)
 
 # set constant -----------------------------------------------
 
+prefix <- "output/simulate/"
+dir.create(
+  prefix, 
+  showWarnings = FALSE, 
+  recursive = TRUE
+)
+
 num_simulation <- 100
 num_alternative <- 3
 num_covariate <- 2
@@ -16,8 +23,23 @@ seed <- 10
 # simulate ----------------------------------------------------
 
 equilibrium <-
+  make_equilibrium(
+    num_simulation = num_simulation,
+    num_alternative = num_alternative,
+    num_covariate = num_covariate
+  )
+
+equilibrium <-
   simulate_choice(
     seed = seed,
     equilibrium = equilibrium
   )
-equilibrium
+
+saveRDS(
+  equilibrium,
+  file = 
+    paste0(
+      prefix, 
+      "equilibrium.rds"
+    )
+)
